d276fd90eea339c0e77ee961a659b76460caabf4
denpo29 <gonza.dp29@hotmail.com>
2023-03-15 16:29:29 -0300

    Subiendo los archivos al repositorio

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let nombre = "Denis"
+let apellido = "Ponce"
+let str_backticks = "Hola esto es un string con backticks"
+let estudiante = nombre.concat(" ", apellido)
+console.log(estudiante)
+let estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
+let estudianteMinus = estudiante.toLowerCase()
+console.log(estudianteMinus)
+let total_letras= estudiante.length
+console.log(total_letras)
+let inicial = estudiante.charAt(0)
+console.log(inicial)
+let final = estudiante.charAt(10)
+console.log(final)
+let sin_espacios = estudiante.trim()
+console.log(sin_espacios)
+let nombre_esta = estudiante.includes("Denis")
+console.log (nombre_esta)
